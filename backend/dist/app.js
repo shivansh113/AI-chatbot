e4c03f45ee11ad667bd7dc40090ba74c43cb5cf3
shivansh113 <shivanshd@gmail.com>
2024-03-25 15:56:28 -0600

    adding basic routing

--- backend/dist/app.js
+++ backend/dist/app.js
@@ -1,7 +1,11 @@
 import express from 'express';
 import { config } from 'dotenv';
+import morgan from 'morgan';
 config();
 const app = express();
 app.use(express.json());
+// remove once application is built
+app.use(morgan("dev"));
+app.use("/api/v1");
 export default app;
 //# sourceMappingURL=app.js.map
